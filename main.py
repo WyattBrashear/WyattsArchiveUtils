332da6aa13532246029071ec1c3c3671b838846a
wyattbrashear <bfamily610@icloud.com>
2026-02-21 11:26:05 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('directory', help="directory to be transformed into an archive")
+parser.add_argument('output', help="output archive name")
+args = parser.parse_args()
+
+dirs_tobe = []
+
+for root, dirs, files in os.walk(args.directory):
+    for file in files:
+        dirs_tobe.append(os.path.join(root, file))
+print(dirs_tobe)
