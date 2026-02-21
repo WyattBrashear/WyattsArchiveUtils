25abfa8d87027235c2d652302d205840928f4162
wyattbrashear <bfamily610@icloud.com>
2026-02-21 11:46:19 -0500

    add 1st testing version

--- writer.py
+++ writer.py
@@ -13,3 +13,10 @@ for root, dirs, files in os.walk(args.directory):
     for file in files:
         dirs_tobe.append(os.path.join(root, file))
 print(dirs_tobe)
+
+with open(args.output, 'ab') as f:
+    for directory in dirs_tobe:
+        f.write(f"!PATH|'{directory}'\n".encode("utf-8"))
+        with open(directory, 'rb') as file:
+            f.write(file.read())
+        f.write("\n!EOF\n".encode("utf-8"))
