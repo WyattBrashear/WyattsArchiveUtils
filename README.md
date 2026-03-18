5439f7ddc229a41eb3c2fc401bf23f2b546e39a4
wyattbrashear <bfamily610@icloud.com>
2026-03-18 17:29:14 -0400

    Add Files

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 this is a project i made (Mostly out of boredom) to make my own archive format. It is pretty much just a Tarball but
 under a different name. 
 
+
+## Installation
+~~~bash
+pip install wyattsarchiveutils
+~~~
+Usage can be seen below.
+
 # Benchmarks
 I know this is what most of you are here for. These tests were done on a 2024 M3 Macbook Air with 16 GB of RAM and 512 GB of Storage (312/494 GB Used). In order to run these tests yourself, Use the -b option when running the scrips
 ## Linux Codebase (As of 2/28/2026 @ 11:19 PM) (Clone depth = 1) (1.87 GB)
@@ -11,6 +18,22 @@ performance of the program to create an archive for it and reconstruct it.
 
 Writing: 15.44 Seconds
 Reconstruction: 1200.21 seconds
+## Usage
+Writer:
+~~~bash
+wau-writer {option}
+~~~
+Options:
+Directory
+Output
+-b (Benchmark)
+Reconstructor:
+~~~bash
+wau-reconstructer {option}
+~~~
+Options:
+Archive
+-b (Benchmark)
 
 ## AI Usage
 This project was assisted in creation by the Pycharm Autocomplete plugin. Along with a small amount of help from Claude (Raycast) when i got stuck.
---
+++ writer/writer.py
@@ -0,0 +1,45 @@
+import os
+import argparse
+import time
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('directory', help="directory to be transformed into an archive")
+    parser.add_argument('output', help="output archive name")
+    parser.add_argument('-b', '--benchmark', action='store_true',
+                        help="Start a timer for running a benchmark on the writer")
+    args = parser.parse_args()
+    start = 0
+    if args.benchmark:
+        start = time.time()
+    dirs_tobe = []
+    files_dirs = []
+    try:
+        for root, dirs, files in os.walk(args.directory):
+            for file in files:
+                dirs_tobe.append(os.path.join(root, file))
+                files_dirs.append(root)
+        with open(f"{args.output}.wam", 'w') as f:
+            f.write("")
+        with open(f"{args.output}.wam", 'ab') as f:
+            f.write("!DIRMAP".encode())
+            is_dir = False
+            for path in files_dirs:
+                f.write(f"\n{path}".encode())
+            f.write("\n!FILEMAP".encode())
+            for path in dirs_tobe:
+                f.write(f"\n{path}".encode())
+            f.write("\n!PAYLOAD".encode())
+            for path in dirs_tobe:
+                with open(path, 'rb') as f2:
+                    file_contents = f2.read()
+                f.write(f"\n!FILE|{path}\n!_!\n".encode())
+                f.write(file_contents)
+        if args.benchmark:
+            print(f"Writer successfully wrote {args.output}.wam in {time.time() - start:.2f}")
+    except:
+        print("An error occured while attempting to write the WAM file")
+if __name__ == "__main__":
+    main()
